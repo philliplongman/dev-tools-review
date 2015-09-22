cd945eae2a43f35bc6a63067b4038051c4fd6947
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-09-22 10:08:25 -0400

    update hound violations

--- spec/features/tools/user_views_index_tools_spec.rb
+++ spec/features/tools/user_views_index_tools_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
-
 feature 'user views tools index page - ', %(
   As an authenticated user
   I want to see a list of all dev tools
   So that I can learn about new dev tools.
   ) do
 
-#  Acceptance Criteria:
-
-#  [x] When I visit the index path I should see a list of dev tool names.
-#  [x] I should see the list of dev tools when I navigate to the root.
+  # Acceptance Criteria:
+  # [x] When I visit the index path I should see a list of dev tool names.
+  # [x] I should see the list of dev tools when I navigate to the root.
 
   scenario 'user visits index page' do
     tool = FactoryGirl.create(:tool)
