fc9b3ec27c16b4f7c8e498d76348f55e403b8e8e
EWarringer <erikwarringer@gmail.com>
2015-09-25 14:55:46 -0400

    completed category relation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'tools#index'
   devise_for :users
 
-  resources :categories, only: [:show]
+  resources :categories, only: [:index, :show]
   resources :tools, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
