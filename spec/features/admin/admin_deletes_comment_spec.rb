7fe8f606cced33a91099999742658b41f9d6ec74
EWarringer <erikwarringer@gmail.com>
2015-09-29 15:23:30 -0400

    Fix tests to include new JS from merge

--- spec/features/admin/admin_deletes_comment_spec.rb
+++ spec/features/admin/admin_deletes_comment_spec.rb
@@ -6,19 +6,21 @@ feature 'Admin deletes comment - ', %(
   So that I can cull the comment population
 ) do
 
-  scenario 'Admin should be able to delete comments' do
+  scenario 'Admin should be able to delete comments', js: true do
     sign_in_admin
     comment = FactoryGirl.create(:comment)
     visit tool_path(comment.review.tool)
+    click_link "1 comment(s)"
     click_button("Delete comment #{comment.id}")
     expect { Comment.find(comment.id) }.
       to raise_error { "Couldn't find Tool with 'id'=#{comment.id}" }
   end
 
-  scenario 'Members should not be able to delete comments' do
+  scenario 'Members should not be able to delete comments', js: true do
     comment = FactoryGirl.create(:comment)
     sign_in
     visit tool_path(comment.review.tool)
+    click_link "1 comment(s)"
     expect(page).to_not have_content("Delete comment #{comment.id}")
   end
 end
--- spec/features/comments/user_deletes_comment_spec.rb
+++ spec/features/comments/user_deletes_comment_spec.rb
@@ -5,18 +5,20 @@ feature "User deletes its own comment" do
     @comment = FactoryGirl.create(:comment)
   end
 
-  scenario "User is able to delete its own comment" do
+  scenario "User is able to delete its own comment", js: true do
     sign_in_specific(@comment.user)
     visit tool_path(@comment.review.tool)
+    click_link "1 comment(s)"
     click_button "Delete comment #{@comment.id}"
     expect { Comment.find(@comment.id) }.
       to raise_error { "Couldn't find Comment with 'id'=#{@comment.id}" }
     expect(page).to have_content("Comment deleted")
   end
 
-  scenario "User is unable to delete another persons comment" do
+  scenario "User is unable to delete another persons comment", js: true do
     sign_in
     visit tool_path(@comment.review.tool)
+    click_link "1 comment(s)"
     expect(page).to_not have_content("Delete comment #{@comment.id}")
   end
 end
