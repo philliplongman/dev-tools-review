863ebf991a0cc72a5634409314b45918419e81a7
Ariel Katz <captainkatz@gmail.com>
2015-10-01 20:29:31 -0400

    fix hound violation

--- spec/features/reviews/new_review_sends_email_to_author_spec.rb
+++ spec/features/reviews/new_review_sends_email_to_author_spec.rb
@@ -4,13 +4,14 @@ feature 'Email alert sent to tool author when new review posted - ', %(
   As an authenticated user
   I want my newly posted review to trigger an email alert to the tool poster
   So that further discussion about the tool can occur
-)do
+) do
 
-# Acceptance Criteria:
-# [x] I must be signed in
-# [x] My valid review submission must cause an email to be sent to the
-#  tool author
-# [x] If I submit an invalid review, no email should be sent to the tool author
+  # Acceptance Criteria:
+  # [x] I must be signed in
+  # [x] My valid review submission must cause an email to be sent to the
+  #    tool author
+  # [x] If I submit an invalid review, no email should be sent to the tool
+  #    author
   scenario "New review on tool sends email to tool poster" do
     ActionMailer::Base.deliveries.clear
     tool = FactoryGirl.create(:tool)
